6b6dabf8068d4f628a2953975c0dac4a4a78dc0d
cloudrohu <cloudrohu@gmail.com>
2024-02-01 12:43:01 +0530

    setting

--- home/views.py
+++ home/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 from business.models import City, Category
+from home.models import Setting
 
 
 def index(request):
     #category = categoryTree(0,'',currentlang)
     city = City.objects.all()
     category = Category.objects.all()
-
+    setting = Setting.objects.all().order_by('-id')[0:1]
 
     context={
         'category':category,
-        'city':city,
-        
+        'city':city,        
+        'setting':setting,        
     }
-
     return render(request, 'index.html',context)
